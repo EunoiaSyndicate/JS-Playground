9ebf3ee4d551a22dc8a5068147cdc9ecd9c08f7b
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-12-18 12:16:44 +0200

    feature: implemented basic layout
    - added button in main activity
    - implemented home activity with top bar and cards
    - added montserrat fonts
    - added card layout
    - implemented card factory (adapter)
    - configured theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-
+  implementation("androidx.cardview:cardview:1.0.0")
   implementation("androidx.core:core-ktx:1.9.0")
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("com.google.android.material:material:1.10.0")
---
+++ app/src/main/java/com/example/js_playground/CardAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.js_playground
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.setMargins
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class CardAdapter(private val context: Context, private val items: List<CardData>) :
+  RecyclerView.Adapter<CardAdapter.ViewHolder>() {
+
+  class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    init {
+      val layoutParams = itemView.layoutParams as GridLayoutManager.LayoutParams
+      layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+      layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
+      layoutParams.setMargins(30)
+      itemView.layoutParams = layoutParams
+    }
+
+    val cardTitle: TextView = itemView.findViewById(R.id.card_title)
+    val cardStatus: TextView = itemView.findViewById(R.id.card_status)
+    val cardDifficultly: TextView = itemView.findViewById(R.id.card_difficulty)
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    val view = LayoutInflater.from(parent.context).inflate(R.layout.card_view, parent, false)
+    return ViewHolder(view)
+  }
+
+  override fun getItemCount(): Int {
+    return items.size
+  }
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    val item = items[position]
+    val status = context.getString(R.string.status)
+    val difficultly = context.getString(R.string.difficultly)
+
+    holder.cardTitle.text = item.title
+    holder.cardStatus.text = String.format("%s: %s",status, item.status)
+    holder.cardDifficultly.text = String.format("%s: %s", difficultly, item.difficultly)
+  }
+}

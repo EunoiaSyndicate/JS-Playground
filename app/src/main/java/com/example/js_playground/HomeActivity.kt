b9d36563bcdf030686f57f66816c103cd4e8affe
SharpDevOps10 <td090833@gmail.com>
2023-12-25 22:41:46 +0200

    feature #5: implemented logic for toolbar buttons
    - added dark/light mode
    - added go to home activity
    - added bookmarks
    - added menu
    - added about us
    
    closes #5

--- app/src/main/java/com/example/js_playground/HomeActivity.kt
+++ app/src/main/java/com/example/js_playground/HomeActivity.kt
@@ -1,6 +1,10 @@
 package com.example.js_playground
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,11 +13,40 @@ import kotlinx.serialization.decodeFromString
 import java.nio.charset.StandardCharsets
 
 class HomeActivity : AppCompatActivity() {
+  private lateinit var themeManager: ThemeManager
+  private lateinit var bookmarkManager: BookmarkManager
+  private lateinit var bookmarkButton: ImageButton
+  private lateinit var menuButton: ImageButton
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    themeManager = ThemeManager(this)
+    themeManager.applyTheme()
     setContentView(R.layout.activity_home)
     val taskList = getCardList()
     fillRecyclerView(taskList)
+
+    bookmarkButton = findViewById(R.id.bookmark_button)
+    bookmarkManager = BookmarkManager(this)
+    bookmarkButton.setOnClickListener { bookmarkManager.showFavoritesMenu(it) }
+    themeManager.setLightModeClickListener(R.id.light_mode_button)
+    themeManager.updateLightModeButton(R.id.light_mode_button)
+
+    val infoButton = findViewById<ImageButton>(R.id.info_button)
+    infoButton.setOnClickListener {
+      val intent = Intent(this, AboutUsActivity::class.java)
+      startActivity(intent)
+    }
+
+    val toMainButton = findViewById<ImageButton>(R.id.to_home_button)
+    toMainButton.setOnClickListener {
+      val intent = Intent(this, MainActivity::class.java)
+      startActivity(intent)
+    }
+
+    menuButton = findViewById(R.id.menu_button)
+    menuButton.setOnClickListener {
+      showPopupMenu(it)
+    }
   }
 
   private fun fillRecyclerView(taskList: TaskList) {
@@ -30,5 +63,33 @@ class HomeActivity : AppCompatActivity() {
     inputStream.close()
     val jsonString = String(buffer, StandardCharsets.UTF_8)
     return Json.decodeFromString<TaskList>(jsonString)
+  }
+
+  private fun showPopupMenu(view: View) {
+    val popupMenu = PopupMenu(this, view)
+    popupMenu.inflate(R.menu.menu_home)
+
+    popupMenu.setOnMenuItemClickListener { item ->
+      when (item.itemId) {
+        R.id.action_bookmarks -> {
+          bookmarkManager.showFavoritesMenu(view)
+          true
+        }
+        R.id.action_about_us -> {
+          startActivity(Intent(this, AboutUsActivity::class.java))
+          true
+        }
+        R.id.action_change_theme -> {
+          themeManager.toggleTheme()
+          true
+        }
+        R.id.action_to_main -> {
+          startActivity(Intent(this, MainActivity::class.java))
+          true
+        }
+        else -> false
+      }
+    }
+    popupMenu.show()
   }
 }
--- app/src/main/java/com/example/js_playground/TaskActivity.kt
+++ app/src/main/java/com/example/js_playground/TaskActivity.kt
@@ -1,9 +1,13 @@
 package com.example.js_playground
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.amrdeveloper.codeview.CodeView
@@ -16,6 +20,15 @@ class TaskActivity : AppCompatActivity() {
     const val TASK = "task"
   }
 
+  private lateinit var starButton: ImageButton
+  private lateinit var titleTextView: TextView
+  private lateinit var bookmarkButton: ImageButton
+  private var isStarred = false
+  private lateinit var bookmarkManager: BookmarkManager
+  private lateinit var cardId: String
+  private lateinit var popupMenu: PopupMenu
+  private lateinit var menuButton: ImageButton
+  private lateinit var themeManager: ThemeManager
   private lateinit var taskFragment: FrameLayout
   private lateinit var editorFragment: FrameLayout
   private lateinit var testsFragment: FrameLayout
@@ -23,6 +36,8 @@ class TaskActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    themeManager = ThemeManager(this)
+    themeManager.applyTheme()
     setContentView(R.layout.activity_task)
     taskFragment = this.findViewById(R.id.task_fragment)
     editorFragment = this.findViewById(R.id.editor_fragment)
@@ -35,7 +50,46 @@ class TaskActivity : AppCompatActivity() {
     val codeView = this.findViewById<CodeView>(R.id.codeView)
     EditorConfigurator.configure(this, codeView)
     setTaskData(task)
+
+    titleTextView = findViewById(R.id.task_header)
+    cardId = task.title
+    titleTextView.text = task.title
+
+    themeManager.setLightModeClickListener(R.id.light_mode_button)
+    themeManager.updateLightModeButton(R.id.light_mode_button)
+
+    val toHomeButton = findViewById<ImageButton>(R.id.to_home_button)
+    toHomeButton.setOnClickListener {
+      val intent = Intent(this, HomeActivity::class.java)
+      startActivity(intent)
+    }
+
+    bookmarkManager = BookmarkManager(this)
+    starButton = findViewById(R.id.star)
+    starButton.setOnClickListener { toggleStarState() }
+
+    val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+    isStarred = sharedPreferences.getBoolean(cardId, false)
+    updateStarButton()
+
+    bookmarkButton = findViewById(R.id.bookmark_button)
+    bookmarkButton.setOnClickListener { bookmarkManager.showFavoritesMenu(it) }
+
+    popupMenu = PopupMenu(this, bookmarkButton)
+    popupMenu.inflate(R.menu.menu_favorites)
+
+    val infoButton = findViewById<ImageButton>(R.id.info_button)
+    infoButton.setOnClickListener {
+      val intent = Intent(this, AboutUsActivity::class.java)
+      startActivity(intent)
+    }
+
+    menuButton = findViewById(R.id.menu_button)
+    menuButton.setOnClickListener {
+      showPopupMenu(it)
+    }
   }
+
 
   private fun setTaskData(task: Task) {
     val taskHeader = findViewById<TextView>(R.id.task_header)
@@ -71,5 +125,59 @@ class TaskActivity : AppCompatActivity() {
 
   private fun onClickDone() {
     javaScriptExecutor.executeScript()
+  }
+
+  private fun toggleStarState() {
+    isStarred = !isStarred
+
+    val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+    val editor = sharedPreferences.edit()
+    editor.putBoolean(cardId, isStarred)
+    editor.apply()
+
+    if (isStarred) bookmarkManager.addFavoriteTask(cardId)
+    else bookmarkManager.removeFavoriteTask(cardId)
+
+    updateStarButton()
+  }
+
+  private fun updateStarButton() {
+    val drawableResId = if (isStarred) R.drawable.baseline_grade_24
+    else R.drawable.baseline_star_border_24
+
+    starButton.setImageResource(drawableResId)
+  }
+
+  private fun showPopupMenu(view: View) {
+    val popupMenu = PopupMenu(this, view)
+    popupMenu.inflate(R.menu.menu_task)
+
+    popupMenu.setOnMenuItemClickListener { item ->
+      when (item.itemId) {
+        R.id.action_bookmarks -> {
+          bookmarkManager.showFavoritesMenu(view)
+          true
+        }
+        R.id.action_about_us -> {
+          startActivity(Intent(this, AboutUsActivity::class.java))
+          true
+        }
+        R.id.action_change_theme -> {
+          themeManager.toggleTheme()
+          true
+        }
+        R.id.action_to_home -> {
+          startActivity(Intent(this, HomeActivity::class.java))
+          true
+        }
+        R.id.action_add_to_favourites -> {
+          bookmarkManager.addFavoriteTask(cardId)
+          toggleStarState()
+          true
+        }
+        else -> false
+      }
+    }
+    popupMenu.show()
   }
 }
